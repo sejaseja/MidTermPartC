80f3b430e65bdefa0aa06cf4fce192615b815742
Sejal Duhan <sejaseja@sheridancollege.ca>
2024-02-20 13:10:53 -0500

    added my details

---
+++ src/midtermexam_versiona_extensioncode/sejal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Hp
+ */
+public class sejal {
+    //my name is sejal duhan
+    // My username is sejaseja@sheridancollege.ca
+    //my student id id 991728437
+}
